be7d0de398be6fce537d8c28430678d87f8e6c02
NKRI <93743775+NKRIDev@users.noreply.github.com>
2026-08-07 15:01:53 +0200

    feat : create post method

--- alfred-web/src/main.rs
+++ alfred-web/src/main.rs
@@ -1,8 +1,24 @@
-use axum::Router;
-use axum::routing::get;
+use axum::{Json, Router};
+use axum::routing::{get, post};
+use serde::Deserialize;
 
 async fn hello_world() -> &'static str {
     "Hello, World!"
+}
+
+/*
+Body to ask content
+ */
+#[derive(Deserialize)]
+struct AlfredAsk {
+    text: String,
+}
+
+/*
+POST on /ask to recover text input by user
+ */
+async fn ask_handle(Json(payload): Json<AlfredAsk>) {
+    println!("{}", payload.text);
 }
 
 #[tokio::main]
@@ -10,7 +26,8 @@ async fn main() {
     /*
     Init routes API
      */
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new().route("/hello", get(hello_world))
+        .route("/ask", post(ask_handle));
 
     /*
     Start web server
